246b64604f2d98ad539d2373051a6db89b869be2
Mae Yee <mae.cst@hotmail.com>
2015-01-17 19:47:10 -0800

    COMP 4711 - Lab02 - build a master view template and simplify views Signed-off-by:Mae Yee <mae.cst@hotmail.com>

--- application/views/template.php
+++ application/views/template.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Contact - ZiZurz Website Template</title>
+	<title>{pagetitle}</title>
 	<link rel="stylesheet" href="css/style.css" type="text/css">
 </head>
 <body>
@@ -31,29 +31,12 @@
 			</ul>
 		</div>
 	</div>
+    
 	<div id="body">
-		<div class="article">
-			<h3>contact us</h3>
-			<p>
-				This website template has been designed by <a href="http://www.freewebsitetemplates.com/">Free Website Templates</a> for you, for free. You can replace all this text with your own text. You can remove any link to our website from this website template, you're free to use this website template without linking back to us. If you're having problems editing this website template, then don't hesitate to ask for help on the <a href="http://www.freewebsitetemplates.com/forums/">Forums</a>.
-			</p>
-			<ul>
-				<li>
-					<span>Address :</span> 7177 Saint Michael Drive, Los Angeles, CA 90048, United States
-				</li>
-				<li>
-					<span>Office Phone Number :</span> (864) 232-3553
-				</li>
-				<li>
-					<span>Fax Number :</span> (864) 232-4160
-				</li>
-				<li>
-					<span>Email :</span> <a href="http://www.freewebsitetemplates.com/misc/contact">info@zizurz.com</a>
-				</li>
-			</ul>
-		</div>
+            {content}
 	</div>
-	<div id="footer">
+
+        <div id="footer">
 		<div>
 			<div class="connect">
 				<a href="http://freewebsitetemplates.com/go/twitter/" id="twitter">twitter</a>
