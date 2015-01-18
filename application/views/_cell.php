6703198222e4adbca6d7d283b52b68afe0f871d5
Mae Yee <mae.cst@hotmail.com>
2015-01-17 19:07:51 -0800

    COMP 4711 - Lab02 - add CodeIgniter files Signed-off-by:Mae Yee <mae.cst@hotmail.com>

---
+++ application/views/_cell.php
@@ -0,0 +1,15 @@
+<?php
+/*
+The original HTML:
+ 
+    <a href="/rooms">
+        <img src="images/first-class.jpg" alt="Img">
+    </a>
+ 
+Converted to be a view fragment template:
+*/
+?>
+
+<a href="{filename}">
+        <img src="./images/{filename}" alt="{alt}"/>
+</a>
